24155e92f320aebc0f6d7df147a2425c73c6d435
romulolink <romulolink@gmail.com>
2016-08-27 15:22:02 -0300

    Versao inicial

---
+++ Assets/CameraManager.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public enum Cameras
+{
+
+    MAIN,
+    TOP
+}
+
+public class CameraManager : MonoBehaviour {
+
+    
+    public Camera[] cameras;
+
+	void Start () {
+	
+
+	}
+	
+
+	void Update () {
+
+        if (Input.GetKey(KeyCode.Space))
+            SwitchCamera();
+	}
+
+    private void SwitchCamera()
+    {
+        if(cameras[(int)Cameras.MAIN].enabled == true)
+        {
+
+            cameras[(int)Cameras.MAIN].enabled = false;
+            cameras[(int)Cameras.TOP].enabled = true;
+        }
+        else
+        {
+            cameras[(int)Cameras.TOP].enabled = false;
+            cameras[(int)Cameras.MAIN].enabled = true;
+            
+        }
+
+    }
+
+
+}
---
+++ Assets/SlotManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class SlotManager : MonoBehaviour {
+
+    private SpriteRenderer indicator;
+
+	void Start () {
+
+        indicator = transform.Find("Indicator/Image").GetComponent<SpriteRenderer>();
+	
+	}
+	
+
+	void Update () {
+	
+	}
+
+
+    void OnTriggerEnter()
+    {
+        Debug.Log("Detectou colisao");
+        indicator.color = Color.red;
+    }
+
+    void OnTriggerExit()
+    {
+
+        indicator.color = Color.green;
+    }
+}
